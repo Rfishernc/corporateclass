1d63f173681b56079af5552f0a1b435db43e54a0
RichFisher <rich.fishernc@gmail.com>
2019-03-05 13:30:11 -0600

    you know, for kids

---
+++ HudsuckerIndustries/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HudsuckerIndustries
+{
+    class Employee
+    {
+        public string Name { get; }
+        private string JobTitle { get; set; }
+        private DateTime StartDate { get; set; }
+
+        public Employee(string name, string jobTitle, DateTime startDate)
+        {
+            Name = name;
+            JobTitle = jobTitle;
+            StartDate = startDate;
+        }
+    }
+}
--- HudsuckerIndustries/Program.cs
+++ HudsuckerIndustries/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HudsuckerIndustries
 {
     class Program
     {
+        public class Company
+        {
+            public string Name { get; }
+            public DateTime CreatedOn { get; }
+            public List<Employee> Employees { get; set; }
+            Dictionary<string, List<Employee>> EmployeeDict;
+
+            public Company(string name, DateTime createdOn)
+            {
+                Name = name;
+                CreatedOn = createdOn;
+                EmployeeDict = new Dictionary<string, List<Employee>>();
+                EmployeeDict[name + "Employees"] = new List<Employee>();
+            }
+
+            public void AddEmployee(string name, string jobTitle, DateTime startDate)
+            {
+                var newEmployee = new Employee(name, jobTitle, startDate);
+                EmployeeDict[Name + "Employees"].Add(newEmployee);
+            }
+
+            public void RemoveEmployee(string name)
+            {
+                var removeEm = Employees.Find(employee => employee.Name == name);
+                EmployeeDict[Name + "Employees"].Remove(removeEm);
+            }
+
+            public void ListEmployees()
+            {
+                foreach(Employee emp in Employees)
+                {
+                    Console.WriteLine(emp.Name);
+                }
+            }
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
